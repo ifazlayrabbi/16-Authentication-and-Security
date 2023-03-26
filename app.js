17b957d4fe03e0135fae803a1a0c93ac5c1c8343
ifazlayrabbi <ifazlayrabbi@gmail.com>
2023-03-26 11:00:18 +0600

    md5 Hash Encryption + salt

--- app.js
+++ app.js
@@ -38,19 +38,73 @@ app.get('/register', (req, res) => {
 
 ////////////////////////////  md5 Hash Encryption  /////////////////////////////
 
+function hash(){
+  app.post('/register', (req, res) => {
+    const {email, password} = req.body
+    
+    const user = new User({
+      email: email,
+      password: md5(password)
+    })
+    user.save()
+    .then(() => {
+      console.log('New user created.')
+      res.render('secrets')
+    })
+    .catch(err => console.log(err.message))
+  })
+  
+  app.post('/login', (req, res) => {
+    const {email, password} = req.body
+    
+    User.find({email: email})
+    .then(userData => {
+      if(userData[0].password === md5(password)){
+  
+        res.render('secrets')
+      }
+      else {
+        console.log('Username or Password Error!')
+        res.send('<h2>Username or Password Error!</h2>')
+      }
+    })
+    .catch(err => console.log(err.message))
+  })
+}
+// hash()
+
+/////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+////////////////////////////  md5 Hash Encryption + salt  /////////////////////////////
+
 app.post('/register', (req, res) => {
   const {email, password} = req.body
   
-  const user = new User({
-    email: email,
-    password: md5(password)
-  })
-  user.save()
-  .then(() => {
-    console.log('New user created.')
-    res.render('secrets')
-  })
-  .catch(err => console.log(err.message))
+  if(email && password){
+    const user = new User({
+      email: email,
+      password: md5(password+process.env.SALT)
+    })
+    user.save()
+    .then(() => {
+      console.log('New user created.')
+      res.render('secrets')
+    })
+    .catch(err => console.log(err.message))
+  }
+  else{
+    console.log('Email or Password Empty!')
+    res.send('<h2>Email or Password Empty!</h2>')
+  }
 })
 
 app.post('/login', (req, res) => {
@@ -58,20 +112,22 @@ app.post('/login', (req, res) => {
   
   User.find({email: email})
   .then(userData => {
-    if(userData[0].password === md5(password)){
+    if(userData[0].password === md5(password + process.env.SALT)){
 
       res.render('secrets')
     }
     else {
-      console.log('Username or Password Error!')
-      res.send('<h2>Username or Password Error!</h2>')
+      console.log('Email or Password Error!')
+      res.send('<h2>Email or Password Error!</h2>')
     }
   })
-  .catch(err => console.log(err.message))
+  .catch(err => {
+    console.log(err.message)
+    res.send('<h2>Email or Password Error!</h2>')
+  })
 })
 
 /////////////////////////////////////////////////////////////////////////////////
-
 
 
 
